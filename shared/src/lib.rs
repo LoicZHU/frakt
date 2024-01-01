11bc5793a5f8ab79040d324509e04df2aefe4906
Mazene <mmazenezerguine@gmail.com>
2024-01-01 14:24:50 +0100

    core: adding fragement_reuest module

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-  left + right
-}
+// fragementRequest module 
+mod fragement_request;
+pub use fragement_request::FragementRequest;
+pub use fragement_request::FragementRequestBuilder;
 
-#[cfg(test)]
-mod tests {
-  use super::*;
 
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
-  }
-}
+// FragementTask module 
