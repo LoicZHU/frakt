e112660b5f91dd60b99962e4e203b6cd6d8254d2
Mazene <mmazenezerguine@gmail.com>
2024-01-02 15:48:41 +0100

    fix: fixing code after review

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use shared::FragmentRequest;
 use worker::Worker;
 
-fn main() {
-  let mut worker: Worker = Worker::new("localhost".to_string(), "group3".to_string(), 8787);
+fn main() -> Result<(), &'static str> {
+  let mut worker = Worker::new("localhost".to_string(), "group3".to_string(), 8787);
 
   let request = FragmentRequest::builder()
     .with_max_work_load(1000)
     .with_worker_name("worker1".to_string())
-    .build()
-    .unwrap();
+    .build()?;
 
-  let request_str = request
-    .to_json()
-    .expect("error while transforming to json string");
+  let request_str = request.to_json().map_err(|err| {
+    eprintln!("Error transforming to json string: {}", err);
+    "Error transforming to json string"
+  })?;
 
   worker.run_worker(request_str);
+
+  Ok(())
 }
